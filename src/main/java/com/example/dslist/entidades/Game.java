ac20880fde90842cab04d823e89e5905d5d8563c
Marchiori <gabriel.marchiori202@gmail.com>
2024-11-11 22:15:26 -0300

    Game seed

--- src/main/java/com/example/dslist/entidades/Game.java
+++ src/main/java/com/example/dslist/entidades/Game.java
@@ -16,19 +16,26 @@ public class Game {
     private Integer ano;
     private String genero;
     private String plataforma;
+    private Double pontuacao;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String descricaoCurta;
+
+    @Column(columnDefinition = "TEXT")
     private String descricaoLonga;
+
 
     public Game() {
     }
 
-    public Game(Long id, String titulo, Integer ano, String genero, String plataforma, String imgUrl, String descricaoCurta, String descricaoLonga) {
+    public Game(Long id, String titulo, Integer ano, String genero, String plataforma, Double pontuacao, String imgUrl, String descricaoCurta, String descricaoLonga) {
         this.id = id;
         this.titulo = titulo;
         this.ano = ano;
         this.genero = genero;
         this.plataforma = plataforma;
+        this.pontuacao = pontuacao;
         this.imgUrl = imgUrl;
         this.descricaoCurta = descricaoCurta;
         this.descricaoLonga = descricaoLonga;
@@ -72,6 +79,14 @@ public class Game {
 
     public void setPlataforma(String plataforma) {
         this.plataforma = plataforma;
+    }
+
+    public Double getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(Double pontuacao) {
+        this.pontuacao = pontuacao;
     }
 
     public String getImgUrl() {
