afe7349edc940bcb3210b6568b53025a2ae060b3
Marchiori <gabriel.marchiori202@gmail.com>
2024-11-12 21:54:09 -0300

    Domin model

--- src/main/java/com/example/dslist/controllers/GameController.java
+++ src/main/java/com/example/dslist/controllers/GameController.java
@@ -1,15 +1,12 @@
 package com.example.dslist.controllers;
 
 import com.example.dslist.dto.GameMinDTO;
-import com.example.dslist.entidades.Game;
-import com.example.dslist.repositories.GameRepository;
 import com.example.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
---
+++ src/main/java/com/example/dslist/entidades/GameList.java
@@ -0,0 +1,52 @@
+package com.example.dslist.entidades;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+
+    public GameList() {
+    }
+
+    public GameList(Long id, String titulo) {
+        this.id = id;
+        this.titulo = titulo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        GameList gameList = (GameList) obj;
+        return Objects.equals(id, gameList.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
