c751c7d9d4e770ddcd2f294fff9e02b20b582263
Marchiori <gabriel.marchiori202@gmail.com>
2024-11-20 16:02:29 -0300

    Movimentacao de games na lista

--- src/main/java/com/example/dslist/repositories/GameListRepository.java
+++ src/main/java/com/example/dslist/repositories/GameListRepository.java
@@ -2,8 +2,19 @@ package com.example.dslist.repositories;
 
 import com.example.dslist.entidades.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
 
+    /*
+    @Modifying
+    @Query(nativeQuery = true, value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+    void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
+     */
+
+    @Modifying
+    @Query(nativeQuery = true, value = "UPDATE tb_belonging SET posicao = :newPosicao WHERE list_id = :listId AND game_id = :gameId")
+    void updateBelongingPosition(Long listId, Long gameId, Integer newPosicao);
 
 }
--- src/main/java/com/example/dslist/services/GameListService.java
+++ src/main/java/com/example/dslist/services/GameListService.java
@@ -3,7 +3,9 @@ package com.example.dslist.services;
 import com.example.dslist.dto.GameListDTO;
 import com.example.dslist.entidades.Game;
 import com.example.dslist.entidades.GameList;
+import com.example.dslist.projections.GameMinProjection;
 import com.example.dslist.repositories.GameListRepository;
+import com.example.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,10 +18,29 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> buscarTodas() {
         List<GameList> result = gameListRepository.findAll();
         List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
         return dto;
     }
+
+    @Transactional
+    public void movimentacao(Long listId, int posicaoOrigem, int posicaoDestino) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(posicaoOrigem);
+        list.add(posicaoDestino, obj);
+
+        int min = posicaoOrigem < posicaoDestino ? posicaoOrigem : posicaoDestino;
+        int max = posicaoOrigem < posicaoDestino ? posicaoDestino : posicaoOrigem;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
+    }
+
 }
